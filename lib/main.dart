d6e17f45fa40ea36a0411f140b5bbc0ad83edda5
jeasonlaung <605251063@qq.com>
2020-01-14 12:00:14 +0800

    更新

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,34 @@
-import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'dart:io';
+
+import 'package:fluwx/fluwx.dart' as fluwx;
+import './pages/home_page.dart';
+
+import './common/fluwe.dart';
+import 'package:flutter/material.dart';
+import './widgets/flutter_app.dart';
+import './router/router.dart';
+
+void main() async{
+  /// 与初始化必须调用这个函数（在runapp之前调用的初始化必须要有）
+  WidgetsFlutterBinding.ensureInitialized();
+  await Fluwe.init(
+    routesConfig: [],
+    
+  );
+  runApp(MyApp());
+
+  // String appId = '';
+  // if(Platform.isIOS) {
+  //   appId = 'wx300fea7d67c15d0f';
+  // } else if(Platform.isAndroid) {
+  //   appId = '';
+  // }
+  // /// 微信注册，可以放后面，不急用
+  // fluwx.registerWxApi(
+  //   appId: appId
+  // );
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -11,37 +39,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      navigatorKey: Router.navigatorKey,
+      onGenerateRoute: Router.onGenerateRoute,
+      home: FluweApp(
+        child: HomePage()
+      ),
     );
   }
 }
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: PageView(
-          children: <Widget>[
-            Container(
-              child: Text('1'),
-            ),
-            Container(
-              child: Text('1'),
-            ),
-            Container(
-              child: Text('1'),
-            )
-          ],
-        ),
-      )
-      
-    );
-  }
-}
-
